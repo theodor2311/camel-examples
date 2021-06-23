68db472242069b290f4f01e99d9299e6f17db239
theodor2311 <theodor2311@gmail.com>
2021-06-24 00:05:32 +0800

    Change cmael.compoent.paho.serverURLs to string array

--- camel-paho2kafka/src/main/java/com/github/theodor2311/example/paho2kafka/Application.java
+++ camel-paho2kafka/src/main/java/com/github/theodor2311/example/paho2kafka/Application.java
@@ -1,36 +1,26 @@
 package com.github.theodor2311.example.paho2kafka;
 
-
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-
-
 @SpringBootApplication
 public class Application {
 
-
     @Value("${camel.component.paho.serverURIs}")
-    private String serverURIs;
+    private String[] serverURIs;
 
     public static void main(String[] args) {
-
-
         SpringApplication.run(Application.class, args);
     }
 
-
     @Bean("mqtt_options")
     public MqttConnectOptions mqttConnectOptions(){
-
-        String[] URIs = {serverURIs};
         MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
-        mqttConnectOptions.setServerURIs (URIs);
+        mqttConnectOptions.setServerURIs (serverURIs);
         return mqttConnectOptions;
     }
-
 
 }
--- camel-paho2kafka/src/main/java/com/github/theodor2311/example/paho2kafka/CamelRouter.java
+++ camel-paho2kafka/src/main/java/com/github/theodor2311/example/paho2kafka/CamelRouter.java
@@ -1,8 +1,6 @@
 package com.github.theodor2311.example.paho2kafka;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,7 +8,6 @@ public class CamelRouter extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-
 
         from("paho:{{camel.component.paho.topic}}?qos={{camel.component.paho.qos}}&userName={{camel.component.paho.userName}}&password={{camel.component.paho.password}}&connectOptions=#mqtt_options")
                 .to("direct:toKafka")
@@ -20,7 +17,5 @@ public class CamelRouter extends RouteBuilder {
                 .to("kafka:{{topics}}")
                 ;
 
-
     }
-
 }
